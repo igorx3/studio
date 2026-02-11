a1d0ae4a69f6e07ddb7b1f6f3b194c9a5e690527
Igor Rodriguez <realptrades@gmail.com>
2026-02-11 08:24:49 +0000

    Try fixing this error: `Console Error: Firebase API Key is missing. Plea

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -5,7 +5,6 @@ import { initializeApp, getApp, getApps, FirebaseApp } from 'firebase/app';
 import { getAuth, Auth } from 'firebase/auth';
 import { getFirestore, Firestore } from 'firebase/firestore';
 import { getStorage, FirebaseStorage } from "firebase/storage";
-import { firebaseConfig } from "./client";
 import { FirebaseContext } from "./context";
 
 export function FirebaseProvider({ children }: { children: ReactNode }) {
@@ -15,6 +14,15 @@ export function FirebaseProvider({ children }: { children: ReactNode }) {
     const [storage, setStorage] = useState<FirebaseStorage | undefined>(undefined);
 
     useEffect(() => {
+        const firebaseConfig = {
+            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        };
+        
         if (firebaseConfig.apiKey) {
             const appInstance = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
             setApp(appInstance);
