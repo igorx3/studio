6bda4a270ff434a9fc8087dc9794a5d37ca61006
Igor Rodriguez <realptrades@gmail.com>
2026-02-15 06:48:36 +0000

    Firestore está activo y las reglas son allow read, write: if true. El er

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDkzDweHZCjAr8y8dXYdrGngx9nFq3k3UV",
+  authDomain: "studio-4342078919-d97a4.firebaseapp.com",
+  projectId: "studio-4342078919-d97a4",
+  storageBucket: "studio-4342078919-d97a4.firebasestorage.app",
+  messagingSenderId: "784721746797",
+  appId: "1:784721746797:web:f2b27939d4576c8cd8d864"
 };
